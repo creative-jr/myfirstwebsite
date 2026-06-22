1a5d2c75778cfe960dcd7d855e2258833a436cb4
creative-jr <jamiksharipov1999@gmail.com>
2026-06-23 00:48:42 +0500

    Upload all project files

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
+$(document).ready(function() {
+    window.addEventListener('load', () => {
+        const text = document.getElementById('text',);
+        text.classList.add('visible');
+    });
+    window.addEventListener('load', () => {
+        const text = document.getElementById('logo',);
+        text.classList.add('visible');
+    });
+    window.addEventListener('load', () => {
+        const text = document.getElementById('hero',);
+        text.classList.add('visible');
+    });
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const subject = event.target.subject.value;
+        const message = event.target.message.value;
+
+        if (name && email && subject && message) {
+            alert('Сообщение отправлено!');
+            event.target.reset();
+        } else {
+            alert('Пожалуйста, заполните все поля.');
+        }
 
     });
-});
-window.addEventListener('load', () => {
-    const text = document.getElementById('text',);
-    text.classList.add('visible');
-});
-window.addEventListener('load', () => {
-    const text = document.getElementById('logo',);
-    text.classList.add('visible');
-});
-window.addEventListener('load', () => {
-    const text = document.getElementById('hero',);
-    text.classList.add('visible');
-});
-document.getElementById('contact-form').addEventListener('submit', function(event, emailjs) {
-    event.preventDefault();
-
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const subject = event.target.subject.value;
-    const message = event.target.message.value;
-
-    if (name && email && subject && message) {
-        alert('Сообщение отправлено!');
-        event.target.reset();
-    } else {
-        alert('Пожалуйста, заполните все поля.');
-    }
-
 });
